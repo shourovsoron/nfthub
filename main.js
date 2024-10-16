583b2e72fe179b616e5ea6d55b855557bbb66af9
Shourov Soron <shourovhossainsoron@gmail.com>
2024-10-16 13:25:57 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+const swiper = new Swiper('.swiper1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 48,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+  
+  });
+  
+  
